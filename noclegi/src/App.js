b3f159ba490979831d6a0a208228346c24e81b8a
michaszo18 <michaszo18@gmail.com>
2021-02-28 10:33:45 +0100

    funkcja searchHandler

--- noclegi/src/App.js
+++ noclegi/src/App.js
@@ -6,31 +6,38 @@ import Hotels from './components/Hotels/Hotels.js'
 
 class App extends React.Component {
 
+  hotels = [
+    {
+      id: 1,
+      name: "Ritz",
+      city: "Paris",
+      street: "Paryska",
+      rating: 90,
+      description: "Hotel Ritz – jedna z najbardziej reprezentacyjnych budowli przedwojennego Białegostoku. Mieścił się w samym centrum miasta, na rogu ulic Niemieckiej i Instytuckiej pod numerem 680a (obecnie ul. Kilińskiego i Pałacowej), pomiędzy pałacem Branickich, a pałacykiem gościnnym.",
+      img: ""
+    },
+    {
+      id: 2,
+      name: "Hotel Dolina Pilicy",
+      city: "Koniecpol",
+      street: "Żytnia 11",
+      rating: 81,
+      description: "To doskonałe miejsce na wypoczynek. Znajduje się w cichym i spokojnym miejscu, niedaleko centrum Koniecpola. Malownicza okolica z sąsiedztwem Rzeki Pilicy i kompleksów zabytkowych obiektów pałacowych i kościelnych, tworzą doskonałe warunki rekreacyjne.",
+      img: ""
+    }
+  ];
+
   state = {
-    hotels: [
-      {
-        id: 1,
-        name: "Ritz",
-        city: "Paris",
-        street: "Paryska",
-        rating: 90,
-        description: "Hotel Ritz – jedna z najbardziej reprezentacyjnych budowli przedwojennego Białegostoku. Mieścił się w samym centrum miasta, na rogu ulic Niemieckiej i Instytuckiej pod numerem 680a (obecnie ul. Kilińskiego i Pałacowej), pomiędzy pałacem Branickich, a pałacykiem gościnnym.",
-        img: ""
-      },
-      {
-        id: 2,
-        name: "Hotel Dolina Pilicy",
-        city: "Koniecpol",
-        street: "Żytnia 11",
-        rating: 81,
-        description: "To doskonałe miejsce na wypoczynek. Znajduje się w cichym i spokojnym miejscu, niedaleko centrum Koniecpola. Malownicza okolica z sąsiedztwem Rzeki Pilicy i kompleksów zabytkowych obiektów pałacowych i kościelnych, tworzą doskonałe warunki rekreacyjne.",
-        img: ""
-      }
-    ],
+    hotels: [this.hotels]
   }
 
   searchHandler(term) {
     console.log(`From app: ${term}`);
+    const hotels = [...this.hotels].filter(
+      x => (x.name.toLocaleLowerCase()).includes(term.toLocaleLowerCase())
+    );
+    this.setState({ hotels })
+
   }
 
   render() {
